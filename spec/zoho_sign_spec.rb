9f98efe775f3df9487abbf1eacdbf91b15f2b919
Kobus Joubert <kobus.joubert@platform45.com>
2025-03-09 08:54:32 +0200

    Rubocop config

--- spec/zoho_sign_spec.rb
+++ spec/zoho_sign_spec.rb
@@ -2,10 +2,6 @@
 
 RSpec.describe ZohoSign do
   it 'has a version number' do
-    expect(ZohoSign::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+    expect(ZohoSign::VERSION).not_to be_nil
   end
 end
